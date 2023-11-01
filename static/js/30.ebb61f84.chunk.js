22fec066aa4fda8a3a252454b7ab549c38becdf5
VizslaFarm <102323223+Carmen274@users.noreply.github.com>
2023-11-01 20:25:39 +0100

    d

---
+++ static/js/30.ebb61f84.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[30],{1726:function(e,o,n){"use strict";n.r(o),function(e){var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof e?n=e.WebSocket||e.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}.call(this,n(196))}}]);
+//# sourceMappingURL=30.ebb61f84.chunk.js.map
