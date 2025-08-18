6b4d8b854994092298c7482890720bfa359d83f6
VizslaFarm <102323223+Carmen274@users.noreply.github.com>
2025-08-18 09:31:25 +0200

    kkk

---
+++ static/js/29.6a5802a3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[29],{1750:function(e,o,n){"use strict";n.r(o),function(e){var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof e?n=e.WebSocket||e.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}.call(this,n(199))}}]);
+//# sourceMappingURL=29.6a5802a3.chunk.js.map
